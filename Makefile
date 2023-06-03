4d6f390fa4e2bfb1fe297e4f9b0193d93c5634e9
Anderson <ander_alvarenga@hotmail.com>
2023-06-03 12:55:21 -0300

    Makefile atualizado

--- Makefile
+++ Makefile
@@ -1,8 +1,26 @@
+NAME = webserv
 
-all: server
+SRCS = server.cpp
 
-server: server.cpp
-	g++ -g server.cpp -o server
+SDIR = src/
+SRCS_DIR = $(addprefix $(SDIR), $(SRCS))
+OBJS = $(SRCS:.cpp=.o)
 
-fclean:
-	rm -fr server
+CPP = c++
+CPPFLAGS = -Wall -Wextra -Werror -std=c++98
+
+RM = rm -fr
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(CPP) $(CPPFLAGS) $(OBJS) -o $(NAME)
+
+%.o: $(SDIR)%.cpp
+	$(CPP) $(CPPFLAGS) -c $< -o $@
+
+clean:
+	$(RM) $(OBJS)
+
+fclean: clean
+	$(RM) webserv
--- src/server.cpp
+++ src/server.cpp
@@ -25,20 +25,21 @@ std::string get_location(char *buffer)
 	return (str_buffer.substr(first_space + 1, second_space - first_space - 1));
 }
 
-int main(int argc, char const* argv[])
+int main(void)
 {
 	int server_fd, new_socket, valread;
 	struct sockaddr_in address;
 	int opt = 1;
 	int addrlen = sizeof(address);
-	char buffer[1024] = { 0 };
+	char buffer[1024] = {0};
 	std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-	std::string nohello = "HTTP/1.1 400 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+	std::string nohello = "HTTP/1.1 404 Not Found\nContent-Type: text/plain\nContent-Length: 15\n\n404Hello world!";
 	// Creating socket file descriptor
 	// ! socket() creates an endpoint for communication and returns a descriptor.
 	// ! AF_INET: is domain argument, and specify to use IPv4 protocols
 	// ! SOCK_STREAM: is type argument, specify to use two-way, connection-based byte stream
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
 		perror("socket failed");
 		exit(EXIT_FAILURE);
 	}
@@ -50,8 +51,9 @@ int main(int argc, char const* argv[])
 	// ! optname: options are passed to the appropriate protocol
 	// ! optval: buffer where options are to be returned
 	if (setsockopt(server_fd, SOL_SOCKET,
-				SO_REUSEADDR | SO_REUSEPORT, &opt,
-				sizeof(opt))) {
+				   SO_REUSEADDR | SO_REUSEPORT, &opt,
+				   sizeof(opt)))
+	{
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
 	}
@@ -65,32 +67,39 @@ int main(int argc, char const* argv[])
 	// Forcefully attaching socket to the port 8080
 	// ! bind assigns the address specified to the (server_fd) socket
 	// ! bind(sockfd, *addr, adrrlen)
-	if (bind(server_fd, (struct sockaddr*)&address,
-			sizeof(address))
-		< 0) {
+	if (bind(server_fd, (struct sockaddr *)&address,
+			 sizeof(address)) < 0)
+	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
 
 	// ! listen marks server_fd as a passive socket, that could have at max 3 pending connections in the queue
-	if (listen(server_fd, 3) < 0) {
+	if (listen(server_fd, 3) < 0)
+	{
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
 
 	// ! accept extract the first connection request on the queue of the server_fd and return a new fd referring to that socket.
-	if ((new_socket
-		= accept(server_fd, (struct sockaddr*)&address,
-				(socklen_t*)&addrlen))
-		< 0) {
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+							 (socklen_t *)&addrlen)) < 0)
+	{
 		perror("accept");
 		exit(EXIT_FAILURE);
 	}
 	valread = read(new_socket, buffer, 1024);
 	std::string location = get_location(buffer);
 	std::cout << "|" << location << "|" << std::endl;
+	if (strcmp(location.c_str(), "/") == 0)
+	{
+		send(new_socket, hello.c_str(), std::strlen(hello.c_str()), 0);
+	}
+	else
+	{
+		send(new_socket, nohello.c_str(), std::strlen(nohello.c_str()), 0);
+	}
 
-	send(new_socket, hello.c_str(), std::strlen(hello.c_str()), 0);
 	printf("Hello message sent\n");
 
 	// closing the connected socket
